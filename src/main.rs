bb0201d6f8503c6ee4e335f2c2f57bfa6ae7ec59
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-18 05:54:20 -0500

    Compare not working.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::io;
+use std::{io, cmp::Ordering};
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
@@ -12,4 +16,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
