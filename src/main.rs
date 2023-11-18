4312fa519e6802826872350e00528b8458e89226
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-18 05:54:08 -0500

    Command working.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use clap::{arg, ArgMatches, command};
+use clap::{arg, Arg, ArgMatches, command, Command};
 
 fn main() {
-    let matches = get_matches_with_macros();
-    if let Some(style) = matches.get_one::<String>("style") {
-        println!("Value for name: {style}");
+    let matches = Command::new("My Test Program")
+        .version("0.1.0")
+        .author("Hackerman Jones <hckrmnjones@hack.gov>")
+        .about("Teaches argument parsing")
+        .arg(Arg::new("file")
+            .short('f')
+            .long("file")
+            .help("A cool file"))
+        .arg(Arg::new("num")
+            .short('n')
+            .long("number")
+            .help("Five less than your favorite number"))
+        .get_matches();
+
+    let default_file = "input.txt".to_string();
+    let myfile: &String = matches.get_one::<String>("file").unwrap_or(&default_file);
+    println!("The file passed is: {}", myfile);
+
+    let num_str = matches.get_one::<String>("num");
+    match num_str {
+        None => println!("No idea what your favorite number is."),
+        Some(s) => {
+            match s.parse::<i32>() {
+                Ok(n) => println!("Your favorite number must be {}.", n + 5),
+                Err(_) => println!("That's not a number! {}", s),
+            }
+        }
     }
 }
 
